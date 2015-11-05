55bf8ead6f001ba9ce2fb6fd9cefb68f5b5076ff
Dave Lively <davelively14@gmail.com>
2015-11-04 21:47:52 -0500

    Completed ModulesAndFunctions-4 and 5

---
+++ factorial.exs
@@ -0,0 +1,16 @@
+defmodule Factorial do
+  def of(0), do: 1
+  def of(n), do: n * of(n-1)
+end
+
+#From page 47, Exercise: ModulesAndFunctions-4
+defmodule Sum do
+  def of(0), do: 0
+  def of(n), do: n + of(n-1)
+end
+
+#From page 47, Exercise: ModulesAndFunctions-5
+defmodule Gcd do
+  def of(x,0), do: x
+  def of(x,y), do: of(y,rem(x,y))
+end
