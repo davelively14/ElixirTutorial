ec6fb7dbe81ed6acf5e57e9ff3dc10c9be75ee05
Dave Lively <davelively14@gmail.com>
2015-11-04 22:17:51 -0500

    At page 50

--- factorial.exs
+++ factorial.exs
@@ -1,12 +1,16 @@
 defmodule Factorial do
   def of(0), do: 1
-  def of(n), do: n * of(n-1)
+  def of(n) when n > 0 do
+    n * of(n-1)
+  end
 end
 
 #From page 47, Exercise: ModulesAndFunctions-4
 defmodule Sum do
   def of(0), do: 0
-  def of(n), do: n + of(n-1)
+  def of(n) when n > 0 do
+    n + of(n-1)
+  end
 end
 
 #From page 47, Exercise: ModulesAndFunctions-5
