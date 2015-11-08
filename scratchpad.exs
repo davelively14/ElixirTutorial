0d03969e67e68a02ae212c6d2e5a91d63d3319e2
Dave Lively <davelively14@gmail.com>
2015-11-07 19:19:11 -0500

    Page 68

--- scratchpad.exs
+++ scratchpad.exs
@@ -54,11 +54,29 @@ defmodule ModFunc7 do
   def current_dir, do: System.cwd
 end
 
-#From page 62. Method to determine length of list.
+#From page 62+. Method to determine length of list.
 defmodule MyList do
   def len([]), do: 0
   def len([_head|tail]), do: 1 + len(tail)
 
   def square([]), do: []
   def square([head|tail]), do: [head*head|square(tail)]
+
+  def add_1([]), do: []
+  def add_1([head|tail]), do: [head+1|add_1(tail)]
+
+  def map([], _func), do: []
+  def map([head|tail], func), do: [func.(head)|map(tail,func)]
+
+  def sum(list), do: _sum(list,0)
+
+  # private methods
+  defp _sum([], total), do: total
+  defp _sum([head|tail], total), do: _sum(tail, head+total)
+end
+
+#From page 68, Exercise ListsAndRecursion-0
+defmodule LaR0 do
+  def sum([]), do: 0
+  def sum([head|tail]), do: head + sum(tail)
 end
