1b452339379b4a23f06b175d7dfc2ff61564f680
Dave Lively <davelively14@gmail.com>
2015-11-17 19:14:13 -0500

    Page 102

--- scratchpad.exs
+++ scratchpad.exs
@@ -151,7 +151,7 @@ defmodule LaR4 do
     do: _span(from + 1, to, [from | list])
   defp _span(from, to, list)
     when from > to,
-    do: list
+    do: Enum.reverse list
 end
 
 defmodule MyDict do
@@ -305,4 +305,21 @@ defmodule Destruct do
     reports = [ dallas: :hot, minneapolis: :cold, dc: :muggy, la: :smoggy ]
     IO.puts inspect for { city, weather} <- reports, do: { weather, city }
   end
+end
+
+defmodule LaR8 do
+  orders = [
+          [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+          [ id: 124, ship_to: :OK, net_amount:  35.50 ],
+          [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+          [ id: 126, ship_to: :TX, net_amount:  44.80 ],
+          [ id: 127, ship_to: :NC, net_amount:  25.00 ],
+          [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+          [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+          [ id: 120, ship_to: :NC, net_amount:  50.00 ] ]
+
+  tax_rates = [ NC: 0.075, TX: 0.08]
+
+
+
 end
