439dc36dfb6bd56d32336b3ab438c4e548812825
Dave Lively <davelively14@gmail.com>
2015-11-17 22:10:00 -0500

    Page 102

--- pad2.exs
+++ pad2.exs
@@ -1,1 +1,26 @@
 # Chapter 11, Strings and Binaries
+defmodule Str do
+  def it do
+    IO.puts "start"
+    IO.write """
+      my
+      string
+      """
+    IO.puts "end"
+  end
+end
+
+defmodule Sigils do
+  def it do
+    IO.puts inspect "C sigil #{~C[1\n2#{1+2}]}"
+    IO.puts inspect "c sigil #{~c"1\n2#{1+2}"}"
+    IO.puts inspect "S sigil #{~S[1\n2#{1+2}]}"
+    IO.puts inspect "s sigil #{~s/1\n2#{1+2}/}"
+    IO.puts inspect ~W[the c#{'a'}t sat on the mat]
+    IO.puts inspect ~w[the c#{'a'}t sat on the mat]
+    # a = atom, c = character list, s = string
+    IO.puts inspect ~w[the c#{'a'}t sat on the mat]a
+    IO.puts inspect ~w[the c#{'a'}t sat on the mat]c
+    IO.puts inspect ~w[the c#{'a'}t sat on the mat]s
+  end
+end
