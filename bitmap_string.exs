de254c377fd7def87164447582d23f45edaeea67
Dave Lively <davelively14@gmail.com>
2015-12-08 19:27:58 -0500

    Page 276

---
+++ bitmap_string.exs
@@ -0,0 +1,11 @@
+defmodule Bitmap do
+  defstruct value: 0
+
+  defimpl String.Chars do
+    def to_string(value), do: Enum.join(value, "")
+  end
+end
+
+fifty = %Bitmap{value: 50}
+
+IO.puts "Fifty in bits is #{fifty}"
